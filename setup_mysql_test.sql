0f980c29fd910efab5aff182c897c26cf12d9335
kevkatam <kevkatam@gmail.com>
2023-11-21 05:10:27 -0800

    adding setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- script that prepares a MySQL server for the project
+-- creates user hbnb_test, and database hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
